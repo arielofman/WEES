0391c9aba6bf4214bc9301b9b2533e6b34504291
ericofman <eric.ofman@hotmail.com>
2017-06-12 01:23:18 -0400

    Added simple page for outputting regex matches

--- coupon/urls.py
+++ coupon/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from .views import generate, redeem
 
 urlpatterns = [ 
-    url(r'^generate/$', generate, name='generate'), 
+    url(r'^generate/(?P<code>[a-zA-Z0-9_]*)/$', generate, name='generate'), 
     url(r'^redeem/$', redeem, name='redeem'),  
 ]
--- coupon/views.py
+++ coupon/views.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 
 # Create your views here.
-def generate(request):
-	return render(request, "coupon/generate.html")
+def generate(request, **kwargs):
+	print(kwargs)
+	return render(request, "coupon/generate.html", {"code": kwargs.get('code')})
 
 def redeem(request):
 	return render(request, "coupon/redeem.html")
