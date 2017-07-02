6c55428a65e4bf4a77bf94b72a31a84949e2522e
ericofman <eric.ofman@hotmail.com>
2017-07-02 15:37:17 -0400

    Added raw coupon code and clean up

--- coupon/templatetags/qr_tags.py
+++ coupon/templatetags/qr_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 def qr_from_text(context, size='100'):    
 	redeem_url = context['request'].build_absolute_uri(reverse('redeem', kwargs={'code': context['code']}))
 	return {"code_url": redeem_url, "size": size}
+
+@register.simple_tag(takes_context=True) 
+def qr_code_text(context): 
+	return context['code']
